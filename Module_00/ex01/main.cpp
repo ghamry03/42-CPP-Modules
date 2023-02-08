fd83c20108e33ef62934c96621412447b45841fa
Omar <omarelghamry@icloud.com>
2023-02-09 03:56:11 +0400

    Added a check for eof in phonebook

--- Module_00/ex01/main.cpp
+++ Module_00/ex01/main.cpp
@@ -6,14 +6,17 @@ int	main(void) {
 	Phonebook	phonebook;
 
 	while (input.compare("EXIT")) {
-		std::cout << "Enter a command: "; 
+		std::cout << "Enter a command: ";
 		std::getline(std::cin, input);
 		if (!input.compare("ADD"))
 			phonebook.addContact();
 		else if (!input.compare("SEARCH"))
 			phonebook.searchContact();
-		else
+		else if (input.compare("EXIT"))
 			std::cout << "unvalid cmd, try using ADD - SEARCH - EXIT" << std::endl;
+		if (std::cin.eof())
+			return (1);
 	}
+	return (0);
 }
 
--- Module_00/ex02/tests.cpp
+++ Module_00/ex02/tests.cpp
@@ -42,8 +42,8 @@ int		main( void ) {
 	ints_t::iterator	wit_begin	= withdrawals.begin();
 	ints_t::iterator	wit_end		= withdrawals.end();
 
-	Account::displayAccountsInfos(); // done
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) ); // done
+	Account::displayAccountsInfos();
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
 	for ( acc_int_t it( acc_begin, dep_begin );
 		  it.first != acc_end && it.second != dep_end;
