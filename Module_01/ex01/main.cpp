f351089b6c997a5224299f552732e3139f41c896
Omar <omarelghamry@icloud.com>
2023-02-14 03:03:35 +0400

    looks better now

--- Module_01/ex01/main.cpp
+++ Module_01/ex01/main.cpp
@@ -1,10 +1,10 @@
 #include "Zombie.hpp"
 
-int	main(void) {
+int	main( void ) {
 	int		num = 69;
 	Zombie	*zombs = zombieHorde( num, "bruh" );
 
-	for (int i = 0; i < num; i++) {
+	for ( int i = 0; i < num; i++ ) {
 		zombs[i].announce();
 	}
 	delete [] (zombs);
