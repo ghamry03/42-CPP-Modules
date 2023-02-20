d9b13f7becc77c846aff7e750c167773c78ffa51
ghamry03 <ommohame@lab1r2s1.42abudhabi.ae>
2023-02-20 20:51:05 +0400

    fixed fall through error in switch statement

--- Module_01/ex06/main.cpp
+++ Module_01/ex06/main.cpp
@@ -6,10 +6,13 @@ int	harlFilter( std::string input ) {
 	switch ( harl.startingLevel( input ) ) {
 		case 0:
 			harl.complain( "DEBUG" );
+ 			// fall through
 		case 1:
 			harl.complain( "INFO" );
+ 			// fall through
 		case 2:
 			harl.complain( "WARNING" );
+ 			// fall through
 		case 3:
 			harl.complain( "ERROR" );
 			break ;
