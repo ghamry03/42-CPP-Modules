23f6557645f80f2573a0fb62a12a24adafe77ad8
Omar <omarelghamry@icloud.com>
2023-02-20 04:34:11 +0400

    added a check if N is negative

--- Module_01/ex01/zombieHorde.cpp
+++ Module_01/ex01/zombieHorde.cpp
@@ -2,8 +2,11 @@
 #include <iostream>
 
 Zombie	*zombieHorde( int N, std::string name ) {
+	if ( N < 0 ) {
+		std::cout << "Invalid number" << std::endl;
+		return ( NULL );
+	}
 	Zombie	*zombs = new Zombie[N];
-
 	for ( int i = 0; i < N; i++ ) {
 		zombs[i].setName(name);
 	}
