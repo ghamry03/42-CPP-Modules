160a0d741c089ed1ed6203112af521bccb1e4160
Omar <omarelghamry@icloud.com>
2023-03-01 17:25:18 +0400

    ex01 done

--- Module_02/ex01/Fixed.cpp
+++ Module_02/ex01/Fixed.cpp
@@ -1,21 +1,22 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed( void ) : _num( 0 ) {
+Fixed::Fixed( void ) {
+    this->setRawBits( 0 );
     std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed( Fixed const& copy ) {
-    std::cout << "Copy constructor called" << std::endl;
     *this = copy;
+    std::cout << "Copy constructor called" << std::endl;
 }
 
 Fixed::Fixed( const int num ) {
-    this->_num = num << _bitsNum;
+    this->setRawBits( num << _bitsNum );
     std::cout << "Int constructor called" << std::endl;
 }
 
 Fixed::Fixed( const float num ) {
-    this->_num = round( num * ( 1 << _bitsNum ) );
+    this->setRawBits( round( num * ( 1 << _bitsNum ) ));
     std::cout << "Float constructor called" << std::endl;
 }
 
@@ -30,12 +31,10 @@ Fixed&          Fixed::operator=( Fixed const& copy ) {
 }
 
 int             Fixed::getRawBits( void ) const {
-    std::cout << "getRawBits member function called" << std::endl;
     return ( this->_num );
 }
 
 void            Fixed::setRawBits( int const raw ) {
-    std::cout << "setRawBits member function called" << std::endl;
     this->_num = raw;
 }
 
