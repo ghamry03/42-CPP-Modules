3ac674fb98e75a0c5866dce811e0d38016454dcb
Omar <omarelghamry@icloud.com>
2023-03-06 19:52:22 +0400

    class.sh v1.0 done

--- class.sh
+++ class.sh
@@ -1,1 +1,79 @@
+#!/bin/bash
+
+# check number of arguments
+if [ $# -ne 1 ]; then
+    echo "Wrong number of arguments, try ./script className"
+    exit 1
+fi
+
+
+# class name from the first argument
 className=$1
+
+# check if incs directory exists in the home directory
+if [ -d "$PWD/incs" ]; then
+    hppPath="$PWD/incs/$className.hpp"
+else
+    hppPath="$PWD/$className.hpp"
+fi
+
+# check if srcs directory exists in the home directory
+if [ -d "$PWD/srcs" ]; then
+    cppPath="$PWD/inc/$className.cpp"
+else
+    cppPath="$PWD/$className.cpp"
+fi
+
+# make sure that class files don't exist already
+if [ -f "$cppPath" ]; then
+    echo "$className cpp file already exists"
+    exit 1
+fi
+if [ -f "$hppPath" ]; then
+    echo "$className hpp file already exists"
+    exit 1
+fi
+
+classNameUpper=$(echo $className | awk '{print toupper($0)}')
+### HPP FILE
+echo "#ifndef ${classNameUpper}_HPP
+# define ${classNameUpper}_HPP
+
+# include <iostream>
+
+class Fixed {
+    private:
+
+    public:
+        $className( void );
+        ~$className( void );
+        $className( Fixed& copy );
+        $className & operator=( Fixed const& copy );
+
+};
+
+#endif
+" >> $hppPath
+
+### CPP FILE
+echo "#include "$className.hpp"
+
+$className::$className( void ) : {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+$className::~$className( void ) {
+    std::cout << "Destructor called" << std::endl;
+}
+
+$className::$className( $className & copy ) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = copy;
+}
+
+$className & $className::operator=( $className const & copy ) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    return ( *this );
+}
+" >> $cppPath
+
