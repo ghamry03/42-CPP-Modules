48be596ec80e69d00dc5fc35f99f584c7e59f5fc
Omar <omarelghamry@icloud.com>
2023-03-06 22:05:13 +0400

    makefile.sh v1.0 done

---
+++ makefile.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# check number of arguments
+if [ $# -ne 1 ]; then
+    echo "Wrong number of arguments, try ./script programName"
+    exit 1
+fi
+
+programName=$1
+
+# check if Makefile already exists
+if [ -f "$PWD/Makefile" ]; then
+    echo "Makeile already exists"
+    exit 1
+fi
+
+echo 'NAME		=	'"$programName"'
+
+SRCS		=	main.cpp			\
+
+OBJS		=	$(SRCS:.cpp=.o)
+
+CXX			=	c++
+
+CXXFLAGS	=	-Wall -Werror -Wextra -std=c++98
+
+RM			=	rm -f
+
+$(NAME)		:	$(OBJS)
+				$(CXX) $(CXXFLAGS) $(OBJS) -o $(NAME)
+
+all			:	$(NAME)
+
+clean		:
+				$(RM) $(OBJS)
+
+fclean		:	clean
+				$(RM) $(NAME)
+
+re			:	fclean all
+' >> Makefile
