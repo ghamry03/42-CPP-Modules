186ba4014a0977471bb7cb2aa0edbe72e98aec90
Omar <omarelghamry@icloud.com>
2023-04-01 16:24:09 +0400

    Module 07 done

--- Module_07/ex02/Array.hpp
+++ Module_07/ex02/Array.hpp
@@ -3,20 +3,21 @@
 #include <exception>
 #include <iostream>
 
-template < typename T >
+template <typename T>
 class Array {
     private:
-        T * _array;
+                T * _array;
         unsigned int _size;
 
     public:
-        Array( void ) : _array( NULL ), _size( 0 ) {
+        Array( void ) : _size( 0 ) { 
             std::cout << "Array default constructor is called" << std::endl;
+            this->_array = new T [0]();
         }
 
         Array( const unsigned int & size ) : _size( size ){
             std::cout << "Array constructor is called" << std::endl;
-            this->setArray( new T [_size]() );
+            this->_array = new T [_size]();
         }
 
         Array( const Array & copy ) {
@@ -32,26 +33,27 @@ class Array {
 
         Array & operator=( const Array & copy ) {
             std::cout << "Array copy assignment operator is called" << std::endl;
-            this->setArray( copy.getArray() );
+
+            if ( this->_array )
+                delete [] this->_array;
+
+            this->_array = new T [ copy.size() ]();
+            for ( unsigned int i = 0; i < copy.size(); i++ ) {
+                this->_array[i] = copy._array[i];
+            }
+            this->_size = copy.size();
+
             return ( *this );
         }
 
         T & operator[]( unsigned int idx ) {
             if ( idx >= _size )
                 throw InvalidIndexException();
-            return ( this->getArray()[idx] );
+            return ( this->_array[idx] );
         }
 
-        T * getArray( void ) const {
-            return ( this->_array );
-        }
-
-        unsigned int getSize( void ) const {
+        unsigned int size( void ) const {
             return ( this->_size );
-        }
-
-        void setArray( T * array ) {
-            this->_array = array;
         }
 
         class InvalidIndexException : public std::exception {
--- Module_07/ex02/main.cpp
+++ Module_07/ex02/main.cpp
@@ -2,26 +2,30 @@
 
 int main(void) {
     {
-        Array <int> * test1 = new Array <int> (10); 
+        Array <int> * test = new Array <int> (10); 
 
-        std::cout << (*test1)[1] << std::endl;
-        (*test1)[1] = 69;
-        std::cout << (*test1)[1] << std::endl;
-        delete test1;
+        std::cout << (*test)[1] << std::endl;
+        (*test)[1] = 69;
+        std::cout << (*test)[1] << std::endl;
+        delete test;
     }
-   
     std::cout << std::endl;
-
     {
-        Array <std::string> test2( 2 );
+        Array <std::string> test( 2 );
         try {
-            test2[0] = "test 0";
-            test2[1] = "test 1";
-            test2[2] = "test 2";
+            test[0] = "test 0";
+            test[1] = "test 1";
+            test[2] = "test 2";
         } catch ( std::exception & e ) {
             std::cout << e.what() << std::endl; 
+            std::cout << "size of array: " << test.size() << std::endl;
         }
-        std::cout << test2[0] << std::endl;
+        std::cout << test[0] << std::endl;
+        std::cout << test[1] << std::endl;
+        Array <std::string> test2;
+        test2 = test;
         std::cout << test2[1] << std::endl;
+        std::cout << "test size: " << test.size()
+            << " test2 size: " << test2.size() << std::endl;
     }
 }
